52a274463e21fe77bd74b1b2231fe68e065b1283
Mark Walker <mark.walker620@gmail.com>
2016-01-20 10:31:56 -0800

    Fixed bug

--- markov.py
+++ markov.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import random
 
 line_starters = []
@@ -17,20 +18,22 @@ def buildDictionaryFromFile():
             if index != len(wordsInLine)-1:
                 word_combos[word].append(wordsInLine[index+1])
             index+=1
+    file.close()
     return
 
 def generateRandomSentence():
-    min_length = 15
     first_word = line_starters[random.randint(0, len(line_starters)-1)]
     sequence = first_word
     choice = word_combos[first_word][random.randint(0, len(word_combos[first_word])-1)]
     while choice not in line_enders:
         sequence+= " " + choice
         choice = word_combos[sequence.split(" ")[-1]][random.randint(0, len(word_combos[sequence.split(" ")[-1]])-1)]
+    sequence+= " " + choice
     return sequence
 
 buildDictionaryFromFile()
 print generateRandomSentence()
+print line_enders
 
 
 
